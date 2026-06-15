c80364649d1782052449f82f37bf78e959059a5c
Jorge2026-cloud <jorgeluislopez347@gmail.com>
2026-06-14 21:58:11 -0600

    Update server.js

--- server.js
+++ server.js
@@ -270,7 +270,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`InterConsumo Masivo running on port ${PORT}`);
 });
